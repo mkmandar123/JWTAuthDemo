1994a96b56fcee62616d1d56a13e51575d91d541
Mandar <mkmandar123@gmail.com>
2020-07-04 11:31:06 +0530

    added comments

--- app.js
+++ app.js
@@ -10,20 +10,32 @@ app.get("/",(req,res)=>{
 	res.send("HOME")
 })
 
+
+
+//Route for login
 app.post("/login",(req,res)=>{
     const{username,password}=req.body
+
+		//validating user
     if(username=='admin'&&password=='admin'){
 
-        const token=jwt.sign({username},"secretKey")
+				//generating a JWT
+        const token=jwt.sign({username},"secretKey",{expiresIn:30})
         res.send(token)
     }
+
+		//Invalid user
     else{
         res.send("Invalid credentials")
     }
 })
 
+
+//some restricted route
 app.post("/restricted",(req,res)=>{
     const {token}=req.body
+
+		//verifying the JWT
     jwt.verify(token,"secretKey",(err,decoded)=>{
         if(err)
             res.send(err)
@@ -33,6 +45,7 @@ app.post("/restricted",(req,res)=>{
 })
 
 
-app.listen(process.env.PORT,()=>{
+//Starting the server
+app.listen(process.env.PORT || 3000,()=>{
     console.log("The server has been started")
 })
